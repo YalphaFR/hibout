cad7500b098fce87b1042126821dfb1d218d66e8
yalpha <44028028+YalphaFR@users.noreply.github.com>
2021-08-05 22:37:01 +0200

    Update say.js

--- src/Commands/say.js
+++ src/Commands/say.js
@@ -1,10 +1,11 @@
 const bot = require("../bot");
 const utils = require("../utils");
+const config = require("../config");
 
 bot.registerCommand("say", (msg, args) => {
     msg.channel.createMessage({embed: {
         color: 0x7da27e,
-        description: args.join(" ")
+        description: msg.content.slice(config.prefix.length)
     }});
     }, {
         argsRequired: true,
