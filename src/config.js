8b70f0e4423346db51dd8d9a6564a5544add0e95
yalpha <44028028+YalphaFR@users.noreply.github.com>
2021-08-08 15:27:18 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const config = {
     token: "Bot " + process.env.TOKEN,
     prefix: "+",
     ownerID: "322492552601272320",
-    status: "dnd", 
+    status: "online", 
     game: {name: "le chant des oiseaux", type: 2},
     
     get requiredModPermissions() {
